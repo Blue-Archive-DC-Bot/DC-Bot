314ed0090ce436275106035a946989037c089f6b
Berted <bertedberted@gmail.com>
2023-12-20 22:12:08 +0700

    add pipenv and pre-commit hooks

--- bot.py
+++ bot.py
@@ -2,28 +2,29 @@ import discord
 import os
 from dotenv import load_dotenv
 
+
 async def send_message(message, user_message, is_private):
     try:
-        await message.channel.send('Under Development')
-    except Exception as e:
+        await message.channel.send("Under Development")
+    except Exception:
         pass
+
 
 def run_discord_bot():
     load_dotenv()
-    token = os.getenv('TOKEN')
+    token = os.getenv("TOKEN")
     client = discord.Client(intents=discord.Intents.default())
     print(token)
 
     @client.event
     async def on_ready():
-        print(f'Bot is now running')
+        print("Bot is now running")
 
     @client.event
     async def on_message(message):
         if message.author == client.user:
             return
-        
+
         await send_message(message, "", False)
-        
-    
+
     client.run(token)
