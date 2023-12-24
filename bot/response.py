29d5edce6746da036efcc2156bf3b889182430f5
Elben85 <elbert.benedict@gmail.com>
2023-12-24 17:06:01 +0700

    Add Variant Data For Char Command

--- bot/response.py
+++ bot/response.py
@@ -14,6 +14,8 @@ def handle_char_command(detail):
     for data in students:
         if data["student"].lower() != detail:
             continue
+        variants = data["variants"]
+        has_variants = len(variants) > 0
         message_content = f"""Name: {data['student']}
 Rarity: {data['rarity']}*
 Role: {data['role']}
@@ -21,6 +23,7 @@ Class: {data['class']}
 Position: {data['position']}
 ATK Type: {data['ATK type']}
 DEF Type: {data['DEF type']}
+Variants: {', '.join(variants) if has_variants else 'None'}
         """
 
         file = File(f"data/char image/{data['student']}.png")
