58db3f972b50d6431d2ad874d803655de05886dc
Elben85 <elbert.benedict@gmail.com>
2023-12-27 18:39:13 +0700

    Make Char Command More Flexible

--- bot/command/char_command.py
+++ bot/command/char_command.py
@@ -2,27 +2,45 @@ from discord import File
 import json
 
 
-def handle_char_command(detail):
+def get_matching_student(query):
+    """
+    Return student where query is a prefix of the students name.
+    If there are multiple, return the student with shortest name.
+    Otherwise returns None.
+    """
     with open("data/students.json") as f:
         students = json.load(f)
 
-    for data in students:
-        if data["student"].lower() != detail:
-            continue
-        variants = data["variants"]
-        has_variants = len(variants) > 0
-        message_content = f"""Name: {data['student']}
-Rarity: {data['rarity']}*
-Role: {data['role']}
-Class: {data['class']}
-Position: {data['position']}
-ATK Type: {data['ATK type']}
-DEF Type: {data['DEF type']}
+    matched = []
+
+    for stu in students:
+        if stu["student"].lower().startswith(query):
+            matched.append(stu)
+
+    if len(matched) == 0:
+        return None
+
+    return min(matched, key=lambda s: len(s["student"]))
+
+
+def handle_char_command(detail):
+    student = get_matching_student(query=detail)
+
+    if student is None:
+        return "Char Not Found", []
+
+    variants = student["variants"]
+    has_variants = len(variants) > 0
+    message_content = f"""Name: {student['student']}
+Rarity: {student['rarity']}*
+Role: {student['role']}
+Class: {student['class']}
+Position: {student['position']}
+ATK Type: {student['ATK type']}
+DEF Type: {student['DEF type']}
 Variants: {', '.join(variants) if has_variants else 'None'}
-        """
+    """
 
-        file = File(f"data/char image/{data['student']}.png")
+    file = File(f"data/char image/{student['student']}.png")
 
-        return message_content, [file]
-
-    return "Char Not Found", []
+    return message_content, [file]
